d263fe9e8a1c82ab7c7cc6f3d4fdc03a8b493a7b
Amory <108595086+amorychambers@users.noreply.github.com>
2024-03-13 23:36:36 +0000

    add function to github-information.js

---
+++ assets/js/github-information.js
@@ -0,0 +1,12 @@
+function fetchGitHubInformation(event) {
+    const username = $('#gh-username').val();
+    if(!username) {
+        $('#gh-user-data').html(`<h2>Please enter a GitHub Username</h2>`);
+        return;
+    };
+    $('#gh-user-data').html(
+        `<div id='loader'>
+            <img src='assets/css/loader.gif' alt='loading...' />
+        </div>`
+    );
+};
